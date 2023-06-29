0989fb20be6c521c9ff1289ad2e6a4f638e115ea
ivanakelovic <ivanakelovic1@gmail.com>
2023-06-29 10:24:23 +0200

    vendors - fronted stranice

---
+++ admin/src/app/pages/helper_functions/customFileUploadButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {useState} from "react";
+
+const CustomFileUploadButton=()=>{
+
+    return(
+      <div></div>
+    );
+}
+
+export default CustomFileUploadButton;
--- admin/src/app/pages/vendors/TenderProposals.tsx
+++ admin/src/app/pages/vendors/TenderProposals.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import clsx from 'clsx'
+import customFileUploadButton from "../helper_functions/customFileUploadButton";
 
 const TenderProposals=()=>{
 
     return(
-      <div>
-          <form className='form w-100'>
+      <div className='d-flex justify-content-center'>
+          <div className='mx-auto col-10 col-md-8 col-lg-6'>
+
+          <form className='form card p-3'>
 
               <h1 className='text-center text-dark'>Make an offer</h1>
 
+              <div className='fv-row mb-10'>
               <label className='form-label fs-6 fw-bolder text-dark'>Tender: </label>
               <br/>
-              <select>
+              <select className={clsx('form-control form-select form-select-lg form-control-solid')}>
                   <option value='0'>Choose</option>
 
               </select>
@@ -27,25 +31,42 @@ const TenderProposals=()=>{
                   )}
 
               />
+              </div>
+
               <br/><br/>
 
-              <label className='form-label fs-6 fw-bolder text-dark'>Your offer: </label><br/>
-              <input
-                  type='number'
+              <label className='form-label fs-6 fw-bolder text-dark'>Your offer:</label>
+              <div className='input-group mb-3'>
+                  <input
+                  className='form-control form-control-lg form-solid'
+                  type='text'
                   placeholder='Your offer'
-                  autoComplete='off'
+                  aria-describedby="basic-addon2"
+                  />
+                  <div className='input-group-append'>
+                      <span className='input-group-text'>$</span>
+                  </div>
+
+              </div>
+
+              <br/><br/>
+
+              {/*
+              <label className='form-label fs-6 fw-bolder text-dark'>File input:</label><br/>
+              <input type="file"
+              className={clsx('form-control form-control-lg form-control-solid')}
               />
+              */}
               <br/><br/>
 
-              <label className='form-label fs-6 fw-bolder text-dark'>Documents:</label><br/>
-              <input type="file"/>
-              <br/><br/>
-
-
-
+              <button type='submit' className='btn btn-lg w-100 mb-5'>
+                Submit
+              </button>
 
 
           </form>
+
+          </div>
       </div>
     );
 };
