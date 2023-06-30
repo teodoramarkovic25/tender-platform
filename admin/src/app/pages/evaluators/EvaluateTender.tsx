4cbfa6335aaef07e3c778456b7a1d69c335f831f
Rada Mastilović <rada.mastilovic94@gmail.com>
2023-06-30 09:37:23 +0200

    evaluate tender form

--- admin/src/app/pages/evaluators/EvaluateTender.tsx
+++ admin/src/app/pages/evaluators/EvaluateTender.tsx
@@ -1,34 +1,62 @@
 import React from "react";
+import clsx from "clsx";
 
 export function EvaluateTender() {
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="mx-auto col-10 col-md-8 col-lg-6">
+        <form className="form card p-3">
+          <h1 className="text-center text-dark">Evaluate Tender</h1>
+         
+           <div className="fv-row mb-10">
+            <label className="form-label fs-6 fw-bolder text-dark">Proposal:</label>
+            <br />
+            <select
+              id="Proposal"
+              name="proposal"
+              className={clsx(
+                "form-control form-select form-select-lg form-control-solid"
+              )}
+            >
+              <option value="proposal1">Proposal 1</option>
+              <option value="proposal2">Proposal 2</option>
+              <option value="proposal3">Proposal 3</option>
+            </select>
+            <br /><br />
+            <label className="form-label fs-6 fw-bolder text-dark">Rating:</label><br />
+            <input
+              type="number"
+              id="rating"
+              name="rating"
+              min="1"
+              max="5"
+              className={clsx(
+                "form-control form-control-lg form-control-solid"
+              )}
+            />
+          </div>
+          <br />
+          <label className="form-label fs-6 fw-bolder text-dark">Comment:</label>
+          <textarea id="comment" name="comment"></textarea>
+          <br /><br />
+          <label className="form-label fs-6 fw-bolder text-dark">Collaborators:</label><br />
+          <input
+            type="text"
+            id="collaborators"
+            name="collaborators"
+            className={clsx(
+              "form-control form-control-lg form-control-solid"
+            )}
+          />
+          <br /><br />
+          <button type="submit" className="btn btn-lg w-100 mb-5">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
 
-    return (
-        <div className='d-flex justify-content-center'>
-          <h1 className='text-center text-dark'>Evaluate Tender</h1>
-          <form className='form card p-3'>
-          <div className='mx-auto col-10 col-md-8 col-lg-6'>
-          <label className='form-label fs-6 fw-bolder text-dark' htmlFor="title">Title:</label>
-          <input className='form-control form-control-lg form-solid' type="text" id="title"required/>
-          <br/>
-          <label className='form-label fs-6 fw-bolder text-dark' htmlFor="description">Description:</label>
-          <input className='form-control form-control-lg form-solid'type="text" id="description" required/>
-          <br/>
-          <label className='form-label fs-6 fw-bolder text-dark'htmlFor="deadline">Deadline date:</label>
-          <input type="date" id="deadline" required/>
-          <br/>
-          <label className='form-label fs-6 fw-bolder text-dark' htmlFor="documents">Associated Documents:</label>
-          <input className='form-control form-control-lg form-solid'type="file" id="documents" required/>
-          <br/>
-          <h3>Evaluation Criteria:</h3>
-           <input className='form-control form-control-lg form-solid' type="text" required/>
-          <br/>
-          <input className='form-control form-control-lg form-solid' type="number" step="0.01" min="0" max="100" placeholder="Weightage" required />
-          <br/>
-          <button className='btn btn-lg w-100 mb-5' type="button" >Create Tender</button>
-          </div>
-          </form>
-          <div>
-          </div>
-          </div>
-      )
-  }
+
+
+  );
+}
