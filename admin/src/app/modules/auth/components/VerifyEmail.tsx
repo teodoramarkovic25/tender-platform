572ec402c6dcfb950dec62f61a7c041043ff9b6e
ivanakelovic <ivanakelovic1@gmail.com>
2023-08-24 12:21:58 +0200

    email verification

--- admin/src/app/modules/auth/components/VerifyEmail.tsx
+++ admin/src/app/modules/auth/components/VerifyEmail.tsx
@@ -1,13 +1,51 @@
 import React from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+import AuthService from "../../../shared/services/api-client/auth.service";
+import {showErrorMessage} from "../../../shared/components/messages/error-createtender-message";
+import {showSuccessMessage} from "../../../shared/components/messages/success-createtender-message";
 
-const VerifyEmail=()=>{
+const VerifyEmail = () => {
 
-    return(
-        <div>
+    const authService = new AuthService();
 
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const token = searchParams.get("token");
+    console.log('token ', token);
+
+    const navigate = useNavigate();
+
+    const verify = async () => {
+        try {
+            if (token) {
+                const verificationResult = await authService.verifyEmail(token);
+                if (verificationResult) {
+                    console.log("Email verified successfully");
+                    showSuccessMessage('Successfully verified email!!! Please log in again!')
+                    navigate('/my-profile');
+                } else {
+                    console.error("Email verification failed");
+                    showErrorMessage('Email verification failed');
+                }
+            } else {
+                console.error("No token found");
+                showErrorMessage('Email verification failed');
+            }
+        } catch (error) {
+            console.error("Error verifying email:", error);
+            showErrorMessage('Email verification failed');
+        }
+    };
+
+
+    return (
+        <div className='d-flex justify-content-center align-items-center'>
+            <button
+                className='btn btn-lg'
+                onClick={verify}>Verify email
+            </button>
         </div>
-    )
+    );
 };
-
 
 export default VerifyEmail;
